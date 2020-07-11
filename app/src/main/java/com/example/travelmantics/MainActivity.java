8ea8e37e861b32b6cd94330db60d593960445e7d
Lawal Azeez <azeezco1@gmail.com>
2020-07-11 20:21:21 +0100

    Write to Realtime Firebase

--- app/src/main/java/com/example/travelmantics/MainActivity.java
+++ app/src/main/java/com/example/travelmantics/MainActivity.java
@@ -4,19 +4,69 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mDatabaseReference;
+    EditText txtTitle;
+    EditText txtDescription;
+    EditText txtPrice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mDatabaseReference = mFirebaseDatabase.getReference().child("traveldeals");
+        txtTitle = findViewById(R.id.txtTitle);
+        txtDescription = findViewById(R.id.txtDescription);
+        txtPrice = findViewById(R.id.txtPrice);
     }
 
+    @Override
+    public  boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.save_menu:
+                saveDeal();
+                Toast.makeText(this,"Deal saved",Toast.LENGTH_LONG).show();
+                clean();
+               // backToList();
+                return  true;
+            case R.id.delete_menu:
+                //deleteDeal();
+                Toast.makeText(this,"Deal deleted", Toast.LENGTH_LONG).show();
+                //backToList();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.save_menu, menu);
         return true;
+    }
+
+    private void saveDeal(){
+        String title = txtTitle.getText().toString();
+        String description = txtDescription.getText().toString();
+        String price = txtPrice.getText().toString();
+        TravelDeal deal = new TravelDeal(title,description,price,"");
+        mDatabaseReference.push().setValue(deal);
+    }
+
+    private void clean() {
+        txtTitle.setText("");
+        txtPrice.setText("");
+        txtDescription.setText("");
+        txtTitle.requestFocus();
     }
 }
